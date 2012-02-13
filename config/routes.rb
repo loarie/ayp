6d8831556c14a6c28d7856dff22441d17849fb06
loarie <loarie@gmail.com>
2012-02-13 10:39:45 -0800

    Fleshed out layout and added some routes

--- config/routes.rb
+++ config/routes.rb
@@ -31,7 +31,7 @@ ActionController::Routing::Routes.draw do |map|
   #   end
 
   # You can have the root of your site routed with map.root -- just remember to delete public/index.html.
-  # map.root :controller => "welcome"
+  map.root :controller => 'pages', :action => 'home'
 
   # See how all your routes lay out with "rake routes"
 
@@ -44,5 +44,6 @@ ActionController::Routing::Routes.draw do |map|
   map.contact '/contact', :controller => 'pages', :action => 'contact'
   map.about   '/about',   :controller => 'pages', :action => 'about'
   map.help    '/help',    :controller => 'pages', :action => 'help'
-  map.root :controller => 'pages', :action => 'home'
+  
+  map.signup '/signup',   :controller => 'users', :action => 'new'
 end
